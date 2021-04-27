612429799f477fecf4a0381d17aeb29161e4f2e7
Saddam-tech <salokhiddinov0727@gmail.com>
2021-04-28 05:32:14 +0900

    final changes

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '541e17f7-8606-4a73-8d41-19cd183368dc';
+export const MODAL_CLOSE_SEC = 2.5;
